bf09397ca00450427de11e27b2d7c967b1267281
Muyiwa Adebayo <muyiwa_david36@yahoo.com>
2025-12-16 13:28:06 +0100

    Add: Added JavaScript math logic and source to HTML

---
+++ script.js
@@ -0,0 +1,22 @@
+const name = "David Adebayo";
+let age = 30;
+
+function add(num1, num2){
+    return num1 + num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function division(num1, num2){
+    return num1 / num2;
+}
+
+let result1 = add(20,10);
+let result2 = multiply(10,2);
+let result3 = division(20,10);
+
+console.log("Addition result: ", result1)
+console.log("Mulitplication result: ", result2)
+console.log("Division result: ", result3)
