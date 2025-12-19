abab8ae3efd65b598df13a37bea772fb2caf7c55
Muyiwa Adebayo <muyiwa_david36@yahoo.com>
2025-12-19 06:16:10 +0100

    Feat: Implemented the DOM and Eventlistener to the Resume

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+"use strict";
+
+
+/**
+ * Calculator Logic
+ */
 const name = "David Adebayo";
 let age = 30;
 
@@ -20,3 +26,17 @@ let result3 = division(20,10);
 console.log("Addition result: ", result1)
 console.log("Mulitplication result: ", result2)
 console.log("Division result: ", result3)
+
+
+// DOM interactive Title
+
+const mainTitle = document.querySelector('header h1')
+
+mainTitle.innerHTML = "David's Dynamic Resume"
+
+const header = document.querySelector('header')
+
+header.addEventListener('click', function(){
+    header.style.backgroundColor = "darkblue"
+    alert("Header colour changed")
+})
